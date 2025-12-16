1948290c9f8a2db413278baa524dc3e9fdaeebd0
ganeshtangade <tangadeganesh02@gmail.com>
2025-12-16 17:45:28 +0530

    Subject wise Pass/Fail

--- Demo_Projects/StudentsMarksCalculator.java
+++ Demo_Projects/StudentsMarksCalculator.java
@@ -21,32 +21,53 @@ public class StudentsMarksCalculator {
 
         int Total = math + science + history + geography + computer ;
         double Percentage = Total/(5.0);
+        
+        //pass fail per subject 
+        String mathResult = (math>=35) ? "Pass" : "Fail";
+        String scienceResult = (science>=35) ? "Pass" : "Fail";
+        String historyResult = (history>=35) ? "Pass" : "Fail";
+        String geographyResult = (geography>=35) ? "Pass" : "Fail";
+        String computerResult = (computer>=35) ? "Pass" : "Fail";
+
+        String overallResult;
+         if (math < 40 || science < 40 || history < 40 || geography < 40 || computer < 40) {
+            overallResult = "FAIL";
+        } else {
+            overallResult = "PASS";
+        }
+
 
         String grade;
-        if(Percentage>=90){
-              grade ="A+";
-        }
-        else if(Percentage>=75){
+        if (overallResult.equals("FAIL")) {
+            grade = "N/A";
+       } else if (Percentage >= 90) {
+            grade = "A+";
+        } else if (Percentage >= 75) {
             grade = "A";
-        }
-        else if(Percentage>=65){
-            grade = "B+";
-        }
-        else if(Percentage>=55){
-            grade = "B+";
-        }
-        else if(Percentage>=35){
-            grade = "C+";
-        }
-        else{
-            grade = "Fail";
+        } else if (Percentage >= 60) {
+            grade = "B";
+        } else {
+            grade = "C";
         }
 
         System.out.println("\n------ Student Result -------");
         System.out.println("Name        :"+name);
-        System.out.println("Total Marks :"+Total+"/500");
-        System.out.println("Percentage  :"+Percentage+"%");
-        System.out.println("Grade       :"+grade);
+
+        System.out.println("\nSubject-wise Result:");
+        System.out.println("Math       : " + math + " - " + mathResult);
+        System.out.println("Science    : " + science + " - " + scienceResult);
+        System.out.println("History    : " + history + " - " + historyResult);
+        System.out.println("Computer   : " + computer + " - " + computerResult);
+        System.out.println("geography     : " + geography + " - " + geographyResult);
+
+
+         System.out.println("\nTotal Marks: " + Total + "/500");
+        System.out.println("Percentage : " + Percentage + "%");
+        System.out.println("Result     : " + overallResult);
+        System.out.println("Grade      : " + grade);
+
+        sc.close();
+
 
 
 
